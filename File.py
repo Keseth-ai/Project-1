c3de62c3243fe66f4ad89d1c9e6ab133e3a6a480
Keseth-ai <raikeseth@gmail.com>
2024-09-15 13:05:57 +0800

    New Project

--- File.py
+++ File.py
@@ -1,9 +1,6 @@
-name = "Alice"
-
-greeting = 'Hello, ' + name
-
-print(greeting)
-
-greeting = greeting.upper()
-
-print
+a = 15
+b = 12
+print("Type of a:", type(a))
+print("Type of b:", type(b))
+addition = a + b
+print("Addition (a + b):", addition)
